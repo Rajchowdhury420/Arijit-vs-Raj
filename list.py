52fd8e24ee95f25cd4ad003b7a081986fdf6780c
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-05-11 11:43:21 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+# Example 2 
+# The following code is also valid 
+
+person_1 = 18
+person_2 = 20
+person_3 = 12
+
+if ( 
+person_1 >= 18 and
+person_2 >= 18 and
+person_3 < 18
+): 
+	print('2 Persons should have ID Cards') 
