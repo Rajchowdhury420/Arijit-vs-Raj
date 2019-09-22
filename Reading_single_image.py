7da1e6e167b9663a660541e682b21c188b724497
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-09-22 15:02:45 +0530

    Update Reading_single_image.py

--- Reading_single_image.py
+++ Reading_single_image.py
@@ -1,14 +1,5 @@
 def read_single_disk(image_id):
-    """ Stores a single image to disk.
-        Parameters:
-        ---------------
-        image_id    integer unique ID for image
-
-        Returns:
-        ----------
-        image       image array, (32, 32, 3) to be stored
-        label       associated meta data, int label
-    """
+   
     image = np.array(Image.open(disk_dir / f"{image_id}.png"))
 
     with open(disk_dir / f"{image_id}.csv", "r") as csvfile:
